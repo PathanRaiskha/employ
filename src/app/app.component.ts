ef7117d267f470cd0d579a7f1f141b13772dfe86
PathanRaiskha <pathanrais8407@gmail.com>
2025-03-15 13:30:20 +0530

    employ data fetch angular application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EmployComponent } from "./employ/employ.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EmployComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/employ/employ.component.ts
@@ -0,0 +1,89 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-employ',
+  imports: [FormsModule,CommonModule],
+  templateUrl: './employ.component.html',
+  styleUrl: './employ.component.css'
+})
+export class EmployComponent {
+  
+  employObj: any[] = []; // List of companies
+  empobj: any = {}; // Object to store form data
+  isEditMode: boolean = false; // Flag for add/update mode
+  UpdateObj:any={
+    companyID:'',
+    companyName:"",
+    companyAdrress:"",
+    companyMail:""
+  }
+
+
+  http=inject(HttpClient)
+
+  ngOnInit() {
+    this.GetAllEmploy();
+  }
+
+  constructor() {}
+
+  // Open modal for adding a company
+  openModalForAdd() {
+    this.isEditMode = false;
+    this.empobj = {};// Clear the object for a new entry
+   
+  }
+
+  // Open modal for editing an existing company
+  openModalForEdit(empobj: any) {
+    this.isEditMode = true;
+    this.empobj = { ...empobj }; // Clone the object to avoid reference issues
+    
+  }
+
+  GetAllEmploy(){
+    this.http.get<any>("https://localhost:7101/api/employ").subscribe((data)=>{
+      this.employObj=data.result
+    })
+  }
+  
+  deleteEmploy(id:number){
+
+    this.http.delete<any>("https://localhost:7101/api/employ/"+id).subscribe({
+      next: (response) => console.log('Response:', response),
+      error: (err) => console.log('Error:', err)
+    });
+  
+
+  }
+  Update(id:number,item:any){
+      this.http.put<any>("https://localhost:7101/api/employ/"+id ,item).subscribe((data)=>{
+        if (data.success){
+          alert(data.massege);
+          
+  
+        }
+  
+      });
+  }
+
+  AddEmploy(item:any){
+    this.http.post<any>("https://localhost:7101/api/employ",item).subscribe((data)=>{
+      if(data.success){
+       alert(data.massege)
+      this. GetAllEmploy();
+      }
+ 
+     })
+
+  }
+  update(item:any){
+    this.UpdateObj=item
+  }
+  
+}
+
+
